48959185be9621a1c2aca39a77c16d704fa493aa
denygurzhiy <denys.gurzhiy@gmail.com>
2022-10-25 19:55:29 +0200

    Craete buttons

--- client/components/button/button.mjs
+++ client/components/button/button.mjs
@@ -1,6 +1,6 @@
 /* Exported, publicly available function, use wherever needed in code */
 
 export function useButton() {
-  const button = document.getElementById('button');
+  const button = document.getElementById('buttonPrimary');
   button.onclick = () => console.log('clicked');
 }
