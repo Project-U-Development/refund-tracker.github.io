cf9259b198078999ec4edc0c54254e2f7ae5f9b8
Elena17T <torgasheva17elena@gmail.com>
2022-11-04 09:25:11 +0100

    changed files and created new function which can help open datapicker by click on div block

--- client/components/datapicker/datapicker.mjs
+++ client/components/datapicker/datapicker.mjs
@@ -69,3 +69,23 @@ document.querySelector('.next').addEventListener('click', () => {
 })
 
 renderCalendar();
+const elements = document.getElementsByClassName('container');
+for(i=0; i<elements.length; i++) {
+    elements[i].addEventListener('mousedown', showDatapicker);
+    elements[i].addEventListener('mouseleave', hideDatapicker);
+}
+
+function showDatapicker() {
+    if(this.children.length>1){
+        this.children[1].style.height = 'auto';
+        this.children[1].style.opacity = '1';
+        this.children[1].style.overflow = 'visible';  
+    }
+}
+function hideDatapicker() {
+    if(this.children.length>1){
+        this.children[1].style.height = '0';
+        this.children[1].style.opacity = '0';
+        this.children[1].style.overflow = 'hidden';  
+    }
+}
