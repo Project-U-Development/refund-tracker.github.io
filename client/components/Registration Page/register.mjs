4e996c410f7525359052fd197b41e99b6784ac2d
IrinaVapniar <116981663+IrinaVapniar@users.noreply.github.com>
2022-11-22 18:21:33 +0100

    RegistrationPage add checking of input validation + small changes in  input.css

--- client/components/Registration Page/register.mjs
+++ client/components/Registration Page/register.mjs
@@ -1,8 +1,23 @@
 function showPasswordFunction() {
-    var show = document.getElementById("password1");
-    if (show.type === 'password') {
-      show.type = 'text';
-    } else {
-      show.type = "password";
-    }
-  }
+   const inputPasswordFirst = document.getElementById("password1");
+   const inputPasswordSecond = document.getElementById("password2");
+   const Status = (input) => (input.type === 'password') ? 'text' : 'password';
+   inputPasswordFirst.type = Status(inputPasswordFirst);
+   inputPasswordSecond.type = Status(inputPasswordSecond);
+}
+
+
+const inputs = document.getElementsByClassName('registerFormInput');
+for (let item of inputs) {
+   item.addEventListener('change', checkInputValidation)
+}
+
+function checkInputValidation() {
+   let validate = true;
+   for (let item of inputs) {
+      if (!item.checkValidity()) {
+         validate = false;
+      }
+   }
+   (validate) ? document.getElementById('createAccount').disabled = false : document.getElementById('createAccount').disabled = true;
+}
