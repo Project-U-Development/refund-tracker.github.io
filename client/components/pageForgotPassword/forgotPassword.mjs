4eb5986f1c3bebb31ac9deed6f8499d01a2bcc79
TatianaBortnik <tanya@bortnik.org>
2022-12-08 18:33:26 +0200

    fixed button disabling

--- client/components/pageForgotPassword/forgotPassword.mjs
+++ client/components/pageForgotPassword/forgotPassword.mjs
@@ -3,6 +3,6 @@ import { inputListener } from '../input/input.mjs';
 inputListener('inputNoLabel', 'input', checkInputValidation);
 
 function checkInputValidation() {
-    const input = document.querySelector('#fieldEmail');
+    const input = document.querySelector('#eMail');
     document.querySelector('#buttonEnterEmail').disabled = !input.checkValidity();
 }
