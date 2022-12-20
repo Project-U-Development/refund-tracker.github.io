2902fc56b26d927858494c6cfc96f8a919c6c858
vikawal <108365967+vikawal@users.noreply.github.com>
2022-12-20 21:59:16 +0100

    code optimization

--- client/components/pageRegistration/register.mjs
+++ client/components/pageRegistration/register.mjs
@@ -1,8 +1,10 @@
 import { inputListener } from '../input/input.mjs';
 
-inputListener('registerFormInput', 'input', checkInputValidation);
+const inputs = document.getElementsByClassName('registerFormInput');
+const inputCheckbox = document.getElementsByClassName('inputCheckbox');
+inputListener(inputs, 'input', checkInputValidation);
+
 function checkInputValidation() {
-   const inputs = document.getElementsByClassName('registerFormInput');
    let validate = true;
    for (let item of inputs) {
       if (item.id === 'password1') {
@@ -20,7 +22,7 @@ function checkPasswords(item) {
    (item.value != '') ? passwordVerification.setAttribute('pattern', item.value) : passwordVerification.setAttribute('pattern', '');
 }
 
-inputListener('inputCheckbox', 'input', showPassword);
+inputListener(inputCheckbox, 'input', showPassword);
 function showPassword() {
    const inputPasswordFirst = document.getElementById("password1");
    const inputPasswordSecond = document.getElementById("password2");
@@ -28,4 +30,3 @@ function showPassword() {
    inputPasswordFirst.type = getNewType(inputPasswordFirst);
    inputPasswordSecond.type = getNewType(inputPasswordSecond);
 }
-
