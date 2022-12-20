5e37b8a63c07ae9777f66025119d79006a6be589
Lina <linalitvinova92@gmail.com>
2022-12-20 11:28:08 +0100

    added options, position of selectors

--- client/components/selectors/selectors.js
+++ client/components/selectors/selectors.js
@@ -1,41 +1,50 @@
-document.querySelectorAll(".selector-group").forEach((dropdownWrapper) => {
+
+document.querySelectorAll(".select-group").forEach((dropdownWrapper) => {
   const dropdownButton = dropdownWrapper.querySelector(".dropdown-button");
-  const dropdownList = dropdownWrapper.querySelector(".dropdown-list");
+  const rotateButton = dropdownWrapper.querySelector(".dropdown-button");
+   const dropdownList = dropdownWrapper.querySelector(".select-options");
   const dropdownInput = dropdownWrapper.querySelector(".dropdown-input");
   const dropdownListItem = dropdownWrapper.querySelectorAll(
-    ".dropdown-list-item"
+    ".select-option-item" 
   );
-  const selectorBlock = dropdownWrapper.querySelector(".selector-block");
+  const selectorBlock = dropdownWrapper.querySelector(".select-block");
+
+  function closeSelect() {
+      dropdownList.classList.remove("select-options-visible");
+      selectorBlock.classList.remove("select-block-highlighted");
+      dropdownList.classList.remove("select-options-highlighted");
+        rotateButton.classList.remove("rotate");  
+}
   // клик по кнопке открывает и закрывает дропдаун
   dropdownButton.addEventListener("click", function () {
-    dropdownList.classList.toggle("dropdown-list-visible");
-    dropdownList.classList.toggle("dropdown-list-highlighted");
-    selectorBlock.classList.toggle("selector-block-highlighted");
+   
+    dropdownList.classList.toggle("select-options-visible");
+    dropdownList.classList.toggle("select-options-highlighted");
+    selectorBlock.classList.toggle("select-block-highlighted");
+  rotateButton.classList.toggle("rotate");  
+     
+     
   });
-
+ 
   // выбирает значение дропдауна для инпута и закрывает дропдаун
   dropdownListItem.forEach((listItem) => {
     listItem.addEventListener("click", function () {
       dropdownInput.value = this.innerText;
-      dropdownList.classList.remove("dropdown-list-visible");
-      selectorBlock.classList.remove("selector-block-highlighted");
-      dropdownList.classList.remove("dropdown-list-highlighted");
+       closeSelect();
+       
     });
   });
   //   Клик снаружи дропдауна закрывает его
   document.addEventListener("click", (e) => {
     if (e.target !== dropdownButton) {
-      dropdownList.classList.remove("dropdown-list-visible");
-      selectorBlock.classList.remove("selector-block-highlighted");
-      dropdownList.classList.remove("dropdown-list-highlighted");
+         closeSelect();
+     
     }
   });
   // Клик на Tab или Escape закрывает дропдаун
   document.addEventListener("keydown", (e) => {
     if (e.key === "Tab" || e.key === "Escape") {
-      dropdownList.classList.remove("dropdown-list-visible");
-      selectorBlock.classList.remove("selector-block-highlighted");
-      dropdownList.classList.remove("dropdown-list-highlighted");
-    }
+      closeSelect();
+         }
   });
 });
