35ece32316571a6b0b2b44d72f7be80fd2e59f77
vikawal <108365967+vikawal@users.noreply.github.com>
2022-12-21 17:49:58 +0100

    added "show password" option

--- client/components/pageLogIn/logIn.mjs
+++ client/components/pageLogIn/logIn.mjs
@@ -3,7 +3,6 @@ import { inputListener } from '../input/input.mjs';
 const inputs = document.getElementsByClassName('logInFormInput');
 
 inputListener(inputs, 'input', checkInputsValidity);
-
 function checkInputsValidity() {
     let validate = true;
     for (let item of inputs) {
@@ -14,6 +13,13 @@ function checkInputsValidity() {
     logInButton.disabled = !validate;
   }
 
+  const inputCheckbox = document.getElementsByClassName('inputCheckbox');
+  inputListener(inputCheckbox, 'input', showPassword);
+function showPassword() {
+   const inputPasswordLogIn = document.getElementById("passwordLogIn");
+   const getNewType = (input) => (input.type === 'password') ? 'text' : 'password';
+   inputPasswordLogIn.type = getNewType(inputPasswordLogIn);
+}
 // import { inputListener } from '../input/input.mjs';
 
 // inputListener('logInFormInput', 'input', checkInputValidationLogIn);
