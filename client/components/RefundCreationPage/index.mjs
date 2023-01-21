a24067d49716238b24a3f3b2bbc4ae72c32b0fc9
OlenaZH84 <elenazhovtiuk@gmail.com>
2023-01-21 11:56:57 +0200

    add options for open refundtracker page when clicking backbutton, add enable create button when all required fields are filled

--- client/components/RefundCreationPage/index.mjs
+++ client/components/RefundCreationPage/index.mjs
@@ -19,14 +19,33 @@ function mistakeMessageFunction() {
    console.log('It is a mistake message...');
 }
 
-btnActive = document.querySelector('.button');
-input = document.querySelector('.input');
-btnActive.setAttribute('disabled', true);
+// btnActive = document.querySelector('.button');
+// input = document.querySelector('.input');
+// btnActive.setAttribute('disabled', true);
 
-input.oninput = function() {
-    if (input.value.length < 5) {
-        btnActive.setAttribute('disabled', true);
-    } else{
-        btnActive.removeAttribute('disabled');
-    }
-}
+// input.oninput = function() {
+//     if (input.value.length < 5) {
+//         btnActive.setAttribute('disabled', true);
+//     } else{
+//         btnActive.removeAttribute('disabled');
+//     }
+// }
+
+function inputButton()
+            {
+                let textInput1 = document.getElementById('text1').value;
+                let textInput2 = document.getElementById('text2').value;
+                let numberInput1 = document.getElementById('number1').value;
+                let numberInput2 = document.getElementById('number2').value;
+                if (textInput1  && numberInput1 && textInput2 && numberInput2) 
+                   
+                {                
+                    document.getElementById('buttonPrimary').disabled = false;
+                } else {
+                    document.getElementById('buttonPrimary').disabled = true;
+                }
+            }
+inputButton();
+document.getElementById('buttonGhost').onclick = function() {
+    window.location.href = 'redirect-url';
+  };
