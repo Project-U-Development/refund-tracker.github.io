6de87d71a9d76a6f4365521faa9a23c22cb2993d
NastyRain <all_nik_mir@ukr.net>
2023-02-12 20:48:12 +0200

    Add import in .js table

---
+++ client/components/table/index.js
@@ -0,0 +1,11 @@
+import creatRows from "./table.js";
+
+let rowId1 = ['12.12.2022', 'Bag', 'Gucci', '100', '20.12.2022'];
+let rowId2 = ['15.12.2022', 'Shoes', 'Nike', '200', '31.12.2022'];
+
+creatRows(tbody, rowId1, rowId2);
+/*inputListener('input', mistakeMessageFunction);*/
+
+/*function mistakeMessageFunction() {
+   console.log('It is a mistake message...');
+}*/
--- client/components/table/table.js
+++ client/components/table/table.js
@@ -1,28 +1,13 @@
-let row_1 = document.createElement('tr');
-
-let row_1_data_0 = document.createElement('td');
-let row_1_data_1 = document.createElement('td');
-let row_1_data_2 = document.createElement('td');
-let row_1_data_3 = document.createElement('td');
-let row_1_data_4 = document.createElement('td');
-
-row_1_data_0.classList.add("table-cell");
-row_1_data_1.classList.add("table-cell");
-row_1_data_2.classList.add("table-cell");
-row_1_data_3.classList.add("table-cell");
-row_1_data_4.classList.add("table-cell");
-
-row_1_data_0.innerHTML = "12.12.2022";
-row_1_data_1.innerHTML = "Bag";
-row_1_data_2.innerHTML = "Gucci";
-row_1_data_3.innerHTML = "100";
-row_1_data_4.innerHTML = "20.12.2022";
-
-tbody.appendChild(row_1);
-row_1.appendChild(row_1_data_0);
-row_1.appendChild(row_1_data_1);
-row_1.appendChild(row_1_data_2);
-row_1.appendChild(row_1_data_3);
-row_1.appendChild(row_1_data_4);
-
-document.getElementById(tbody).appendChild(row_1);
+export default function creatRows(tbody){
+	
+	let row = document.createElement('tr');
+	tbody.appendChild(row);
+	let i = 0;
+	while (i < 5){
+	let cell = document.createElement('td');
+	row.appendChild(cell);
+	cell.classList.add("table-cell");
+	cell.innerHTML = rowId1[i];
+	i++;
+	}
+}
