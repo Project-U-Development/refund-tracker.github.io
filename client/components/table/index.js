3cd82b744b4ab3e81892d356f1b2fb8a6d5bdce1
TatianaBortnik <tanya@bortnik.org>
2023-02-18 19:56:55 +0200

    delete debug text

--- client/components/table/index.js
+++ client/components/table/index.js
@@ -4,8 +4,3 @@ let rowId1 = ['12.12.2022', 'Bag', 'Gucci', '100', '20.12.2022'];
 let rowId2 = ['15.12.2022', 'Shoes', 'Nike', '200', '31.12.2022'];
 
 creatRows(tbody, rowId1, rowId2);
-/*inputListener('input', mistakeMessageFunction);*/
-
-/*function mistakeMessageFunction() {
-   console.log('It is a mistake message...');
-}*/
--- client/src/homePage.mjs
+++ client/src/homePage.mjs
@@ -19,10 +19,8 @@ function createTableRow(row, table) {
   table.appendChild(tr);
 
 }
-console.log("Hi");
 
 let token = localStorage.getItem("token");
-console.log('-----------------------------', token);
 const apiUrl =
   window.origin === "http://localhost:3000"
     ? "http://localhost:80"
@@ -37,7 +35,6 @@ async function getRefundList() {
     },
   });
   const json = await response.json();
-  console.log(json);
   return json;
 }
 
@@ -46,50 +43,7 @@ async function getRefundList() {
   const result = await getRefundList();
   const list = document.getElementById('tbody');
   list.replaceChildren(); // removes all children (list items) from list, before adding freshly acquired ones
-  console.log(result);
   result.forEach((item) => createTableRow(item, list));
 
 
-// // --------------POST
-// async function postRefund(pName,pDebtor,pAmount,pDueDate) {
-//   const response = await fetch('https://oijv97k0l6.execute-api.eu-central-1.amazonaws.com/test/refunds', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       name: pName,
-//       debtor: pDebtor,
-//       amount: pAmount,
-//       dueDate: pDueDate,
-//       // currency: 'euro' // optional field, you can pass any currenct name, but you don't have to. By default it's in euro.
-//     })
 
-//   });
-//   let result = {
-//     status: response.status,
-//     statusText: response.statusText
-//   }
-//   return result;
-// }
-
-// const form = document.getElementById('detail-form');
-// form.onsubmit = async function submit(event) {
-//   // Preventing page reload
-//   event.preventDefault();
-//   // Creating special object, so that we can get input values
-//   const formData = new FormData(form);
-//   // Getting input values by input names (NOT ids)
-//   const name = formData.get('name') || '';
-//   const debtor = formData.get('debtor') || '';
-//   const amount = formData.get('amount') || '';
-//   const dueDate = formData.get('dueDate') || '';
-
-//   let result = await postRefund(name,debtor,amount,dueDate);
-//   const resultText = document.getElementById("result");
-//   if (result.status === 200) {
-//     resultText.innerHTML = 'Refund created!'
-//   } else if (result.status >= 400) {
-//     resultText.innerHTML = result.statusText;
-//   }
-// }
