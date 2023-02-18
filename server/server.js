23ff7e0e3c10bad8b2cf0f6745913fcc67ff173d
IrinaVapniar <116981663+IrinaVapniar@users.noreply.github.com>
2023-02-18 13:13:13 +0100

    Added server port in ENV

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ const fastify = require("fastify")({ logger: true });
 const cors = require("@fastify/cors");
 
 if (process.env.NODE_ENV === "local") {
-  dotenv.config({ path: "./.env-local" });
+   dotenv.config({ path: "./.env-local" });
 } else {
-  dotenv.config({ path: "./.env" });
+   dotenv.config({ path: "./.env" });
 }
 fastify.register(cors, {
-  origin: "*",
-  methods: ["GET", "POST", "OPTIONS"],
+   origin: "*",
+   methods: ["GET", "POST", "OPTIONS"],
 });
 const { verifyJWT } = require("./controllers/authorization/auth");
 fastify.decorate("verifyJWT", verifyJWT);
@@ -20,16 +20,16 @@ fastify.register(require("./routes/usersRoutes"));
 fastify.register(require("./routes/refundsRoutes"));
 
 const server = async () => {
-  try {
-    const port = 80;
-    await fastify.listen({ port: port, host: "0.0.0.0" });
-    console.info(
-      `Server FASTIFY is working on port ${fastify.server.address().port}...`
-    );
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
+   try {
+      const port = process.env.SERVER_PORT;
+      await fastify.listen({ port: port, host: "0.0.0.0" });
+      console.info(
+         `Server FASTIFY is working on port ${fastify.server.address().port}...`
+      );
+   } catch (err) {
+      fastify.log.error(err);
+      process.exit(1);
+   }
 };
 
 server();
