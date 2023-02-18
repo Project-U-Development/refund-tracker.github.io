706ee1bf9a03bdc00952eb0e00905b75f751e5e0
Admin <admin@Admins-MacBook-Pro.local>
2023-02-18 13:42:33 +0100

    deleted console.log

--- client/src/authentication.js
+++ client/src/authentication.js
@@ -1,6 +1,6 @@
 function main() {
   const token = localStorage.getItem("token");
-  console.log(window.location.pathname);
+
   if (token && token.length > 0 && window.location.pathname === "/login") {
     window.location.href = "/";
   }
