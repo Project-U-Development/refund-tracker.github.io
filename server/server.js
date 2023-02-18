b48ec9e89d5643497297b77a75a388797403adeb
Admin <admin@Admins-MacBook-Pro.local>
2023-02-18 12:10:03 +0100

    added cors

--- server/server.js
+++ server/server.js
@@ -1,31 +1,35 @@
-const dotenv = require('dotenv');
-const fastify = require('fastify')({ logger: true });
+const dotenv = require("dotenv");
+const fastify = require("fastify")({ logger: true });
+const cors = require("@fastify/cors");
 
-if (process.env.NODE_ENV === 'local') {
-   dotenv.config({ path: './.env-local' });
+if (process.env.NODE_ENV === "local") {
+  dotenv.config({ path: "./.env-local" });
+} else {
+  dotenv.config({ path: "./.env" });
 }
-else {
-   dotenv.config({ path: './.env' });
-}
+fastify.register(cors, {
+  origin: "*",
+  methods: ["GET", "POST", "OPTIONS"],
+});
+const { verifyJWT } = require("./controllers/authorization/auth");
+fastify.decorate("verifyJWT", verifyJWT);
 
-const {verifyJWT} = require('./controllers/authorization/auth');
-fastify.decorate('verifyJWT', verifyJWT);
-
-fastify.register(require('./routes/home'));
-fastify.register(require('./routes/version'));
-fastify.register(require('./routes/usersRoutes'));
-fastify.register(require('./routes/refundsRoutes'));
+fastify.register(require("./routes/home"));
+fastify.register(require("./routes/version"));
+fastify.register(require("./routes/usersRoutes"));
+fastify.register(require("./routes/refundsRoutes"));
 
 const server = async () => {
-   try {
-      const port = 80;
-      await fastify.listen({ port: port, host: '0.0.0.0' });
-      console.info(`Server FASTIFY is working on port ${fastify.server.address().port}...`);
-   }
-   catch (err) {
-      fastify.log.error(err);
-      process.exit(1);
-   }
+  try {
+    const port = 80;
+    await fastify.listen({ port: port, host: "0.0.0.0" });
+    console.info(
+      `Server FASTIFY is working on port ${fastify.server.address().port}...`
+    );
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 };
 
 server();
